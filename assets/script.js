6cf627c993e7ecafdd363401f6b85b55f8c40793
Zolnikov Aleksey <monsarda@yandex.ru>
2017-07-31 21:13:37 +0300

    main Logic tic-tac-toe

---
+++ assets/script.js
@@ -0,0 +1,57 @@
+// Tic Tac Toe
+
+"use strict";
+
+function TicTacToe() {
+
+	let collection = document.getElementsByTagName('input');
+	let chip = 'user';
+	this.input = new Array();
+	
+	// --------------- //
+
+	this.init = function() {
+
+		for (var i = 0; i < collection.length; i++) {
+			this.input.push(collection[i]);
+		}
+
+	}
+
+	this.userTurn = function(el) {
+
+		el.parentNode.style.background = '#fafafa';
+		el.checked = true;
+
+		this.pcTurn();
+	}
+
+	this.pcTurn = function() {
+
+		// На последнем шаге зависает
+		do{
+			var random = Math.floor(Math.random() * (8 - 0 + 1)) + 0;
+		} while (this.input[random].checked);
+
+
+		this.input[random].parentNode.style.background = '#ececec';
+		this.input[random].checked = true;
+
+	}
+
+
+}
+
+let game = new TicTacToe();
+game.init();
+
+for (var i = 0; i < game.input.length; i++) {
+	
+	game.input[i].onclick = function() {
+		game.userTurn(this);
+	}
+
+}
+
+
+
