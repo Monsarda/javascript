83b906bbd642c25d065e864af4aa277e71fef61d
Zolnikov Aleksey <monsarda@yandex.ru>
2017-08-01 21:10:13 +0300

    update

--- assets/script.js
+++ assets/script.js
@@ -2,84 +2,20 @@
 
 "use strict";
 
-function TicTacToe() {
+function NoughtsAndCrosses() {
 
-	let collection = document.getElementsByTagName('input');
-	this.input = new Array();
+	const HUMAN_DOT = 'X';
+	const AI_DOT = 'O';
+	const EMPTY_DOT = '*';
+	let field = new Array();
+
 	
-	// --------------- //
 
-	this.init = function() {
-
-		for (var i = 0; i < collection.length; i++) {
-			this.input.push(collection[i]);
-		}
-
-	}
-
-	this.userTurn = function(el) {
-
-		el.parentNode.style.background = '#fafafa';
-		el.checked = true;
-
-	}
-
-	this.pcTurn = function() {
-
-		// На последнем шаге зависает
-		do{
-			var random = Math.floor(Math.random() * (8 - 0 + 1)) + 0;
-		} while (this.input[random].checked);
-
-
-		this.input[random].parentNode.style.background = '#ececec';
-		this.input[random].checked = true;
-
-	}
-
-	this.checkFullArea = function() {
-
-		var counter = 0;
-
-		for (var i = 0; i < this.input.length; i++) {
-			
-			if (this.input[i].checked) {
-				counter++;
-			}
-		}
-		if (counter >= 8) {
-			return false
-		}else{
-			return true;
-		}
-
-	}
 
 }
 
-let game = new TicTacToe();
-game.init();
+let game = new NoughtsAndCrosses();
 
-// for (var i = 0; i < game.input.length; i++) {
-	
-// 	game.input[i].onclick = function() {
-
-// 		game.userTurn(this);
-		
-
-// 		let checkFullArea = game.checkFullArea();
-
-
-// 		if(checkFullArea == true)
-// 		{
-// 			game.pcTurn();
-// 		}else{
-// 			alert('AREA IS FULL');
-// 		}
-		
-// 	}
-
-// }
 
 
 
