f2779c6f3324d5374f6a482cf2c98eb675e16dff
Zolnikov Aleksey <monsarda@yandex.ru>
2017-08-01 21:24:07 +0300

    update

--- assets/script.js
+++ assets/script.js
@@ -7,9 +7,23 @@ function NoughtsAndCrosses() {
 	const HUMAN_DOT = 'X';
 	const AI_DOT = 'O';
 	const EMPTY_DOT = '*';
-	let field = new Array();
+	let field = new Object();
+
+	while (true) {
+		printMap();
+	}
+
 
 	
+
+	function printMap() {
+		console.log('* | 1 | 2 | 3');
+		console.log('1 | * | * | *');
+		console.log('2 | * | * | *');
+		console.log('3 | * | * | *');
+
+		return false;
+	}
 
 
 }
